b913b1ea03dbb1f5c6461dae33c1471457e3d664
Jeremy Ellis <jellis@hpssapps.com>
2014-07-18 22:35:49 -0400

    Create myCommands.sh

---
+++ myCommands.sh
@@ -0,0 +1,35 @@
+echo "Basic Linux ubuntu commands"
+echo ""
+
+echo "To go up a directory"
+echo "cd .."
+echo ""
+
+echo "To enter into a directory or folder. Hit tab after a few letters to auto complete"
+echo "cd dirName  "
+echo ""
+
+echo "To show the present path with directory"
+echo "pwd "
+echo ""
+
+echo "To set every file in this folder to be able to run as a bash command"
+echo "chmod 700 *  "
+echo ""
+
+echo "To make a new directory in the present directory"
+echo "mkdir dirname      "
+echo ""
+
+echo "To list all files in the present directory and show if they are runnable"
+echo "ls -l     "
+echo ""
+
+
+
+
+echo ""
+
+echo ""
+
+echo ""
