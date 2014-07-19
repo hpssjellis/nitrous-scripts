fa1677d0228a5f0eb95798a6b889632fc6628d93
Jeremy Ellis <jellis@hpssapps.com>
2014-07-18 23:03:16 -0400

    Create gitgetandset.sh

---
+++ gitgetandset.sh
@@ -0,0 +1,29 @@
+echo "Make sure you are in the correct directory for your repository"
+
+
+echo "what is your username"
+read myUser
+
+echo "what is your email"
+read myEmail
+
+echo "what is the short directory name for this repo"
+read myDirectory
+
+mkdir $myDirectory
+echo "making directory $myDirectory. Press enter to continue into the directory"
+read myStop
+cd $myDirectory
+
+echo "what is your github repository- note you can use shift -ctrl-v to paste in Nitrous.io"
+read myGit
+
+git init
+git remote add origin $myGit
+git config user.name "$myUser"
+git config user.email "$myEmail"
+git pull origin master
+
+
+echo "remember to copy the file gitpush.sh into your new directory.So that you can return your changes to github."
+echo "To restore github got into the folder and type ./gitpush.sh. you will need your github login and password."
